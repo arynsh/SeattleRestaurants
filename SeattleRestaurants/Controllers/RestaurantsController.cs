ff3275e3c9bddae996a61cd7d5b2637f768c81e5
Sharon Lee <sharonlee714@gmail.com>
2020-03-30 23:56:47 -0700

    add func to query all endpoints

--- SeattleRestaurants/Controllers/RestaurantsController.cs
+++ SeattleRestaurants/Controllers/RestaurantsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SeattleRestaurants.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SeattleRestaurants.Controllers
 {
@@ -11,36 +13,66 @@ namespace SeattleRestaurants.Controllers
     [ApiController]
     public class RestaurantsController : ControllerBase
     {
+        private SeattleRestaurantsContext _db;
+        public RestaurantsController(SeattleRestaurantsContext db)
+        {
+        _db = db;
+        }
         // GET api/restaurants
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Restaurant>> Get(string name, string address, string phone, string cuisinetype)
         {
-            return new string[] { "value1", "value2" };
+            var query = _db.Restaurants.AsQueryable();
+            if (name != null)
+            {
+                query = _db.Restaurants.Where(entry => entry.Name == name);
+            }
+            if (address != null)
+            {
+                query = _db.Restaurants.Where(entry => entry.Address == address);
+            }
+            if (phone != null)
+            {
+                query = _db.Restaurants.Where(entry => entry.Phone == phone);
+            }
+            if (cuisinetype != null)
+            {
+                query = _db.Restaurants.Where(entry => entry.CuisineType == cuisinetype);
+            }
+            return query.ToList();
         }
 
         // GET api/restaurants/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Restaurant> Get(int id)
         {
-            return "value";
+           return _db.Restaurants.FirstOrDefault(entry => entry.RestaurantId == id);
         }
 
         // POST api/restaurants
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Restaurant restaurant)
         {
+            _db.Restaurants.Add(restaurant);
+            _db.SaveChanges();
         }
 
         // PUT api/restaurants/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Restaurant restaurant)
         {
+            restaurant.RestaurantId = id;
+            _db.Entry(restaurant).State = EntityState.Modified;
+            _db.SaveChanges();
         }
 
         // DELETE api/restaurants/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var RestaurantToDelete = _db.Restaurants.FirstOrDefault(entry => entry.RestaurantId == id);
+            _db.Restaurants.Remove(RestaurantToDelete);
+            _db.SaveChanges();
         }
     }
 }
